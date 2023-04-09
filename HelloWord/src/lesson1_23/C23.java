db08aa5fe11c3f01537e2491d8d9db2c92e2ae7a
hasegawatakuto <126218608+hasegawatakuto@users.noreply.github.com>
2023-04-09 17:07:29 +0900

    課題23　再提出　継承のためパッケージを２つに分ける

--- HelloWord/src/lesson1_23/C23.java
+++ HelloWord/src/lesson1_23/C23.java
@@ -43,17 +43,8 @@ public class C23 {
 		this.speed = speed;
 	}
 	
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		C23 lion = new C23();
-		lion.setName("ライオン");
-		lion.setBodylength(2.1);
-		lion.setSpeed(80);
+	
 		
-		System.out.println("動物名：" + lion.getName());
-	    System.out.println("体長：" + lion.getBodylength() + "m");
-	    System.out.println("速度：" + lion.getSpeed() + "km/h");
 
 	}
 
-}
---
+++ HelloWord/src/lesson23_2/Lesson23_2.java
@@ -0,0 +1,19 @@
+package lesson23_2;
+
+import lesson1_23.C23;
+
+public class Lesson23_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		C23 lion = new C23();
+		lion.setName("ライオン");
+		lion.setBodylength(2.1);
+		lion.setSpeed(80);
+		
+		System.out.println("動物名：" + lion.getName());
+	    System.out.println("体長：" + lion.getBodylength() + "m");
+	    System.out.println("速度：" + lion.getSpeed() + "km/h");
+	}
+
+}
