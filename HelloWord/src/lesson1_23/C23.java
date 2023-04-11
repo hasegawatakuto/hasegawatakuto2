357fcb8b627471a87eeb5afcca310470e210e4f1
hasegawatakuto <126218608+hasegawatakuto@users.noreply.github.com>
2023-04-11 21:04:36 +0900

    課題23 再提出　インデント合わせと余分なコード削除

--- HelloWord/src/lesson1_23/C23.java
+++ HelloWord/src/lesson1_23/C23.java
@@ -1,12 +1,12 @@
 /*
- 	
+
  	下記がコンソールに出力されるように作成してください
  	※thisとsetterとgetterとフィールドを使ってください
- 	
+
   	動物名：ライオン
 	体長：2.1m
 	速度：80km/h
- 	
+
  */
 
 package lesson1_23;
@@ -15,9 +15,9 @@ public class C23 {
 	private String name;
 	private double bodyLength;
 	private double speed;
-	 
+
 	public C23() {}
-	
+
 	public C23(String name,double bodyLength,double speed)
 	{this.name = name;
 	this.bodyLength = bodyLength;
@@ -41,10 +41,6 @@ public class C23 {
 	}
 	public void setSpeed(double speed) {
 		this.speed = speed;
-	}
-	
-	
-		
-
-	}
+	}	
+}
 
--- HelloWord/src/lesson23_2/Lesson23_2.java
+++ HelloWord/src/lesson23_2/Lesson23_2.java
@@ -5,15 +5,13 @@ import lesson1_23.C23;
 public class Lesson23_2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		C23 lion = new C23();
 		lion.setName("ライオン");
 		lion.setBodyLength(2.1);
 		lion.setSpeed(80);
-		
-		System.out.println("動物名：" + lion.getName());
-	    System.out.println("体長：" + lion.getBodyLength() + "m");
-	    System.out.println("速度：" + lion.getSpeed() + "km/h");
-	}
 
+		System.out.println("動物名：" + lion.getName());
+		System.out.println("体長：" + lion.getBodyLength() + "m");
+		System.out.println("速度：" + lion.getSpeed() + "km/h");
+	}
 }
