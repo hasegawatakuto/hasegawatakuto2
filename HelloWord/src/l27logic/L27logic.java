5faa23107db365f582074f645db7c4f807b196ae
hasegawatakuto <126218608+hasegawatakuto@users.noreply.github.com>
2023-04-13 00:01:44 +0900

    課題27 提出 4/13

--- HelloWord/src/l27logic/L27logic.java
+++ HelloWord/src/l27logic/L27logic.java
@@ -2,13 +2,13 @@ package l27logic;
 
 public class L27logic {
 	private String name;
-	private double bodylength;
+	private double bodyLength;
 	private int speed;
 	private String name2;
-	
-	public L27logic(String name,double bodylength,int speed,String name2){
+
+	public L27logic(String name,double bodyLength,int speed,String name2){
 		setName(name);
-		setBodylength(bodylength);
+		setBodylength(bodyLength);
 		setSpeed(speed);
 		setName2 (name2);
 	}
@@ -20,10 +20,10 @@ public class L27logic {
 		this.name = name;
 	}
 	double getBodylength() {
-		return this.bodylength;
+		return this.bodyLength;
 	}
-	void setBodylength(double bodylength) {
-		this.bodylength = bodylength;
+	void setBodylength(double bodyLength) {
+		this.bodyLength = bodyLength;
 	}
 	int getSpeed() {
 		return this.speed;
@@ -39,48 +39,46 @@ public class L27logic {
 	}
 	//コンソールに出力する内容
 	public void process(String input) {
-	//sprit文を用いて[,]で区切るように設定したものをanimalsと命名
+		//sprit文を用いて[,]で区切るように設定したものをanimalsと命名
 		String[] animals = input.split(",");
-    //for文を使いコンソールで入力された文章を[:]で分ける。
-	//コンソールで入力されspritで[:]と分けたものを文章はannimaldetailとして命名。
-        for (String animal : animals) {
-            String[] animalDetails = animal.split(":");
-    //それぞれの動物名や体長の情報を配列番号を指定し呼び出している。
-    //parseIntを使用し数字を文字列として設定。理由として実行用パッケージでnextlineを使用するため。
-            String name = animalDetails[0];
-            double bodylength = Double.parseDouble(animalDetails[1]);
-            int speed = Integer.parseInt(animalDetails[2]);
-    //name2は空白にすることでswich文で判定した文字を出力させる
-            String name2 = "";
-	
-    switch (name) {
-        case "ライオン":
-            name2 = "パンテラ レオ";
-            break;
-        case "ゾウ":
-            name2 = "ロキソドンタ・サイクロティス";
-            break;
-        case "パンダ":
-            name2 = "アイルロポダ・メラノレウカ";
-            break;
-        case "チンパンジー":
-            name2 = "パン・トゥログロディテス";
-            break;
-        case "シマウマ":
-            name2 = "チャップマンシマウマ";
-            break;
-        case "インコ":
-            name2 = "不明";
-            break;
-        
-}
-    //コンソールに出力する文章。最後に()を使うことで改行をしている
-    System.out.println("動物名：" + name);
-    System.out.println("体長：" + bodylength + "m");
-    System.out.println("速度：" + speed + "km/h");
-    System.out.println("学名：" + name2);
-    System.out.println();
-    
+		//for文を使いコンソールで入力された文章を[:]で分ける。
+		//コンソールで入力されspritで[:]と分けたものを文章はannimaldetailとして命名。
+		for (String animal : animals) {
+			String[] animalDetails = animal.split(":");
+			//それぞれの動物名や体長の情報を配列番号を指定し呼び出している。
+			//parseIntを使用し数字を文字列として設定。理由として実行用パッケージでnextlineを使用するため。
+			String name = animalDetails[0];
+			double bodyLength = Double.parseDouble(animalDetails[1]);
+			int speed = Integer.parseInt(animalDetails[2]);
+			//name2は空白にすることでswich文で判定した文字を出力させる
+			String name2 = "";
+
+			switch (name) {
+			case "ライオン":
+				name2 = "パンテラ レオ";
+				break;
+			case "ゾウ":
+				name2 = "ロキソドンタ・サイクロティス";
+				break;
+			case "パンダ":
+				name2 = "アイルロポダ・メラノレウカ";
+				break;
+			case "チンパンジー":
+				name2 = "パン・トゥログロディテス";
+				break;
+			case "シマウマ":
+				name2 = "チャップマンシマウマ";
+				break;
+			case "インコ":
+				name2 = "不明";
+				break;
+			}
+			//コンソールに出力する文章。最後に()を使うことで改行をしている
+			System.out.println("動物名：" + name);
+			System.out.println("体長：" + bodyLength + "m");
+			System.out.println("速度：" + speed + "km/h");
+			System.out.println("学名：" + name2);
+			System.out.println();
+		}
 	}
-}
 }
