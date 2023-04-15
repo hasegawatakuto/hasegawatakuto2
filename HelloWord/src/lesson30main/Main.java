dcf3744f8d7e0c5d05eeb08721a8ec321eea3231
hasegawatakuto <126218608+hasegawatakuto@users.noreply.github.com>
2023-04-15 21:18:58 +0900

    課題30 再提出　4/15 コメントアウトの追加

--- HelloWord/src/lesson30main/Main.java
+++ HelloWord/src/lesson30main/Main.java
@@ -16,6 +16,7 @@ package lesson30main;
 import person_30.Person;
 class Main {
 	  public static void main(String[] args) {
+		//それぞれPerson1,2に引数（年齢、身長、体重）を入れる。
 	    Person person1 = new Person("鈴木太郎",20,1.7,86.7);
 	    person1.print();
 	    Person person2 = new Person("山田花子",22,1.5,56.25);
--- HelloWord/src/person_30/Person.java
+++ HelloWord/src/person_30/Person.java
@@ -2,15 +2,15 @@ package person_30;
 
 import bicycle_31.Bicycle;
 import car_31.Car;
-
+    //コントラスタを定義。
 public class Person{
 	static int count = 0;
 	public String name;
 	int age;
 	double height;
 	double weight;
-    double bmi;
-	// コンストラクタを定義しインスタンスフィールドに値をセット
+	double bmi;
+	// コンストラクタを定義しインスタンスフィールドに値をセット。count++でそれぞれの人の数を計算。この問題の場合2人と出力される。
 	public Person(String name,int age, double height,double weight){
 		Person.count++;
 		this.name = name;
@@ -18,21 +18,24 @@ public class Person{
 		this.height = height;
 		this.weight = weight;
 	}
+	//BMIを計算する式を入力し変数bmiにreturnする。
 	public double bmi() {
 		return  this.weight / this.height / this.height ;
 	}
+	//コンソールに出力する文章の型を定義。
 	public void print() {
 		System.out.println("名前は" + this.name + "です");
 		System.out.println("BMI" + this.bmi() + "です");
 		System.out.println("年は" + this.age + "です");
 	}
+	//buyメソッドにCarクラスのオーナーを定義。コンソールに出力する文章を定義。
 	public void buy(Car car) {
 		car.setOwner(this.name);
 		System.out.println(car.getOwner() + "が購入しました");
-		}
-
-		public void buy(Bicycle bicycle) {
+	}
+	//Carクラスと同じくbuyメソッドにbicicleオーナーを定義。コンソールに出力する文章を定義。
+	public void buy(Bicycle bicycle) {
 		bicycle.setOwner(this.name);
 		System.out.println(bicycle.getOwner() + "が購入しました");
-		}
+	}
 }
