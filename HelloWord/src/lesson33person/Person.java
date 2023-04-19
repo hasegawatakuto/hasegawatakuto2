296715c045a70e298b18584f1dee1cf0324c9e86
hasegawatakuto <126218608+hasegawatakuto@users.noreply.github.com>
2023-04-19 23:52:12 +0900

    課題33 再提出 4/19 コメントアウトの追加

--- HelloWord/src/lesson33person/Person.java
+++ HelloWord/src/lesson33person/Person.java
@@ -1,35 +1,35 @@
 package lesson33person;
-
+    // コンストラクタを作成。
 public class Person{
-public static int count = 0;
-public String firstName;
-public String lastName;
-public int age;
-public double height, weight;
-
-public Person(String firstName, String lastName, int age, double height, double weight){
-Person.count++;
-this.firstName = firstName;
-this.lastName = lastName;
-this.age = age;
-this.height = height;
-this.weight = weight;
-}
-
-public String fullName(){
-return this.firstName + this.lastName;
-}
-
-public void print(){
-System.out.println("名前は" + this.fullName() + "です");
-System.out.println("年は" + this.age + "です");
-}
-
-public double bmi(){
-return this.weight / this.height / this.height;
-}
-
-public static void printCount(){
-System.out.println("合計" + Person.count + "人です");
-}
+	public static int count = 0;
+	public String firstName;
+	public String lastName;
+	public int age;
+	public double height, weight;
+	// Personメソッドに引数を入れてcount++で人数の数を追加。
+	public Person(String firstName, String lastName, int age, double height, double weight){
+		Person.count++;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+	// 変数fullNameにfirstNameとlastNameを足した物をリターン分で返す。
+	public String fullName(){
+		return this.firstName + this.lastName;
+	}
+	// printメソッドにコンソールで出力する文を記入。
+	public void print(){
+		System.out.println("名前は" + this.fullName() + "です");
+		System.out.println("年は" + this.age + "です");
+	}
+	// 変数bmiにbmiを計算する式をリターンで返す。
+	public double bmi(){
+		return this.weight / this.height / this.height;
+	}
+	// printCountメソッドに人数の数を出す文書を記入。
+	public static void printCount(){
+		System.out.println("合計" + Person.count + "人です");
+	}
 }
