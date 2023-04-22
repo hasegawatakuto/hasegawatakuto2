9390ca2d36eab8d96afd02a92dd3f5ce10aca469
hasegawatakuto <126218608+hasegawatakuto@users.noreply.github.com>
2023-04-22 22:46:29 +0900

    課題33 再提出　4/23 インデント修正

--- HelloWord/src/lesson33main/Main.java
+++ HelloWord/src/lesson33main/Main.java
@@ -9,16 +9,20 @@ package lesson33main;
 import lesson33person.Person;
 class Main{
 	public static void main(String[] args){
+		
 		// インスタンス生成してperson1にPersonクラスの引数の入った物を代入する。
 		Person person1 = new Person("鈴木", "太郎", 20, 1.7, 60);
+		
 		// Person1をPersonクラスで作ったprintメソッドを使いコンソールに出力する。
 		person1.print();
+		
 		// Person1と同様にインスタンス生成してperson2にPersonクラスの引数の入った物を代入する。
 		Person person2 = new Person("山田", "花子", 22, 1.5, 40);
+		
 		// Person1をPersonクラスで作ったprintメソッドを使いコンソールに出力する。
 		person2.print();
+		
 		// Personクラスで作ったprintCountメソッドを使いコンソールに出力する。
 		Person.printCount();
-		
 	}
 }
--- HelloWord/src/lesson33person/Person.java
+++ HelloWord/src/lesson33person/Person.java
@@ -1,4 +1,5 @@
 package lesson33person;
+
     // コンストラクタを作成。
 public class Person{
 	public static int count = 0;
@@ -6,6 +7,7 @@ public class Person{
 	public String lastName;
 	public int age;
 	public double height, weight;
+	
 	// Personメソッドに引数を入れてcount++で人数の数を追加。
 	public Person(String firstName, String lastName, int age, double height, double weight){
 		count++;
@@ -15,19 +17,23 @@ public class Person{
 		this.height = height;
 		this.weight = weight;
 	}
+	
 	// 変数fullNameにfirstNameとlastNameを足した物をリターン分で返す。
 	public String fullName(){
 		return this.firstName + this.lastName;
 	}
+	
 	// printメソッドにコンソールで出力する文を記入。
 	public void print(){
 		System.out.println("名前は" + this.fullName() + "です");
 		System.out.println("年は" + this.age + "です");
 	}
+	
 	// 変数bmiにbmiを計算する式をリターンで返す。
 	public double bmi(){
 		return this.weight / this.height / this.height;
 	}
+	
 	// printCountメソッドに人数の数を出す文書を記入。
 	public static void printCount(){
 		System.out.println("合計" + count + "人です");
