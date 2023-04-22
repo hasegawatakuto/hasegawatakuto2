01c2f079da43b748fb9185774d64438edcc2210d
hasegawatakuto <126218608+hasegawatakuto@users.noreply.github.com>
2023-04-22 22:45:28 +0900

    課題32 再提出　4/22 インデント修正

--- HelloWord/src/lesson32main/Main.java
+++ HelloWord/src/lesson32main/Main.java
@@ -10,8 +10,7 @@
 問題5：クラスメソッドの中にクラスフィールド「count」を用いて「合計〇〇人です」と出力してください
 
 問題6：クラスメソッド「printCount」を呼び出してください
-*/
-
+ */
 
 package lesson32main;
 
@@ -19,16 +18,21 @@ import lesson32person.Person;
 
 class Main{
 	public static void main(String[] argos){
+		
 		// インスタンス生成してperson1にPersonクラスの引数の入った物を代入する。
 		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
+		
 		// Person1をPersonクラスで作ったprintメソッドを使いコンソールに出力する。
 		person1.print();
+		
 		// Person1と同様にインスタンス生成してperson2にPersonクラスの引数の入った物を代入する。
 		Person person2 = new Person("山田花子", 22, 1.5, 40);
+		
 		// Person1をPersonクラスで作ったprintメソッドを使いコンソールに出力する。
 		person2.print();
+		
 		// Personクラスで作ったprintCountメソッドを使いコンソールに出力する。
 		Person.printCount();
-	
-}
+		
+	}
 }
--- HelloWord/src/lesson32person/Person.java
+++ HelloWord/src/lesson32person/Person.java
@@ -1,4 +1,5 @@
 package lesson32person;
+
     // コンストラクタを作成。
 public class Person{
 	static int count = 0;
@@ -6,24 +7,28 @@ public class Person{
 	public int age;
 	public double height;
 	public double weight;
+	
 	// Personメソッドに引数を入れてcount++で人数の数を追加。
 	public Person(String name, int age, double height, double weight){
 		count++;
-		this.name = name;
-		this.age = age;
+		this.name   = name;
+		this.age    = age;
 		this.height = height;
 		this.weight = weight;
 	}
-    // 変数bmiにbmiを求める式をリターン分で返す。
+	
+	// 変数bmiにbmiを求める式をリターン分で返す。
 	public double bmi(){
 		return this.weight / this.height / this.height;
 	}
-    // printメソッドにmainクラスでコンソールに出す文章を記入。
+	
+	// printメソッドにmainクラスでコンソールに出す文章を記入。
 	public void print(){
 		System.out.println("名前は" + this.name + "です");
 		System.out.println("年は" + this.age + "です");
 	}
-    // printCountメソッドに人数の数を出す文書を記入。
+	
+	// printCountメソッドに人数の数を出す文書を記入。
 	public static void printCount(){
 		System.out.println("合計" + Person.count + "人です");
 	}
